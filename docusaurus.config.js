b5013c7b6be1073da3403b22e8169e3d33df4492
kevinbdx35 <k.bardoux@orange.fr>
2021-11-29 17:13:21 +0100

    update blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
             items: [
               {
                 label: 'Twitter',
-                href: '/',
+                href: 'https://github.com/kevinbdx35',
               },
             ],
           },

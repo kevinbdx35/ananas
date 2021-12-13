b34880bbc279f5fb90b4c20ce28ecbc6b283b61d
kevinbdx35 <k.bardoux@orange.fr>
2021-12-14 00:52:48 +0100

    add PWA Plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -102,6 +102,36 @@ const config = {
       },
     }),
   themes: ['@docusaurus/theme-live-codeblock']
+  plugins: [
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      },
+    ],
+  ],
 };
 
 module.exports = config;
